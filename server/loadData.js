0c6e3256e488fa10d3a4d42aa12a27b6ea3d63c3
Tim Honders <tim.honders@kega.nl>
2023-05-11 16:37:47 +0200

    Product name fix

--- server/loadData.js
+++ server/loadData.js
@@ -14,11 +14,13 @@ const loadData = () => {
         if (data?.data?.GeneralInfo && data?.data?.GeneralInfo?.GTIN[0]) {
             data = data.data;
 
+            console.log(file + ' ' + data.GeneralInfo.ProductName);
+            console.log(data.GeneralInfo.TitleInfo.GeneratedIntTitle)
             products.push({
                 //IcecatId: data.GeneralInfo.IcecatId,
                 GTIN: data.GeneralInfo.GTIN[0],
-                productname: data.GeneralInfo.ProductName,
-                BrandPartCode: data.GeneralInfo.BrandPartCode
+                productname: data.GeneralInfo.TitleInfo.GeneratedIntTitle,
+                BrandPartCode: data.GeneralInfo.BrandPartCode,
                 //brand: data.GeneralInfo.Brand,
                 //title: data.GeneralInfo.Title,
                 //category: data.GeneralInfo.Category,
--- server/server.js
+++ server/server.js
@@ -27,24 +27,40 @@ const port = process.env.PORT || 3001;
 
 app.get('/', async (req, res) => {
     const { query: { q } } = req;
+    
+    const qt = 'Can u make a nice structure of this data';
 
+    const promt = `${ JSON.stringify(products[4]) }
+
+    ${qt}
+    `;
+
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: promt,
+    });
+
+    console.log(completion.data.choices);
+
+   /*
     const prompt = 'Give me GTIN of product with productname s20?';
 
     let productData = products.map((product) => {
-        const { GTIN, productname, BrandPartCode } = product?.GeneralInfo ?? {};
- 
+        const { GTIN, productname, BrandPartCode, Brand } = product?.GeneralInfo ?? {};
+
         return {
             role: 'system',
-            content: `Product: ${ productname }\nBrandPartCode: ${ BrandPartCode }\nGTIN: ${ GTIN }`,
+            content: `Product name: ${ productname }\nBrandPartCode: ${ BrandPartCode }\nGTIN: ${ GTIN }\nBrand: ${Brand}`,
         }
     });
 
     const data = searchChatGPT(prompt, [
-        { role: 'user', content: 'Give me GTIN of product with productname s20?' },
+        { role: 'user', content: 'Give me a product with productname s20 and brand samsung, and give me the GTIN from the data?' },
         ...productData
     ]);
 
     res.json(data);
+    */
 
     /*
     const qt = 'Give me GTIN of product with productname s20 based on the data';
@@ -87,7 +103,7 @@ app.post('/q', async (req, res) => {
         model: "text-davinci-003",
         prompt: promt,
     });
-
+    console.log(promt)
     let GTIN = completion?.data?.choices[0] ? completion?.data?.choices[0]?.text.match(/\d{13}/g) : null;
     GTIN = GTIN && GTIN.join('');
 
@@ -113,16 +129,17 @@ const searchChatGPT = async (prompt, messages) => {
 
     try {
         const response = await openai.createChatCompletion({
-            model: "text-davinci-003",
+            model: "gpt-3.5-turbo",
             messages: messages,
             max_tokens: 100,
             n: 1,
             stop: '\n',
         });
+        
+        if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+            console.log(response.data.choices);
 
-    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
-        const completion = response.data.choices[0].text.trim();
-        return completion;
+            return response.data.choices;
         } else {
             throw new Error('Unexpected response from OpenAI API');
         }
